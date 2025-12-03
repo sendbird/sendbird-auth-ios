f6eb08fabeef01daf2991b5b7bc3ec872589c2c2
Kai Lee <kai.lee@sendbird.com>
2025-12-03 15:10:54 +0900

    Revert package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,6 @@
 // swift-tools-version:5.9
 
+import Foundation
 import PackageDescription
 
 let package = Package(
@@ -15,10 +16,14 @@ let package = Package(
         ),
     ],
     targets: [
-        .binaryTarget(
+        .target(
             name: "SendbirdAuthSDK",
-            url: "https://github.com/sendbird/sendbird-auth-ios/releases/download/0.0.3/SendbirdAuthSDK.xcframework.zip",
-            checksum: "8a3861a9bd55e220d16262acf1858eaca269786f8beaa59a386549a942bec8c4"
+            path: "Sources/SendbirdAuth",
+            swiftSettings: swiftSettings
         ),
     ]
 )
+
+var swiftSettings: [SwiftSetting] = [
+    .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
+]
