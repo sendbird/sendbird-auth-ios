b55a4566700c4303a515cab96e554f06faf04dc4
Kai Lee <kai.lee@sendbird.com>
2026-01-27 11:00:29 +0900

    Merge pull request #21 from sendbird/feat/kai/extendable-urlpath
    [CLNP-7930] Make `URLPaths` extendable

---
+++ Sources/SendbirdAuth/Common/Commands/Base/RequestParameter.swift
@@ -0,0 +1,46 @@
+//
+//  RequestParameter.swift
+//  SendbirdAuthSDK
+//
+//  Created by Kai Lee on 1/21/26.
+//
+
+import Foundation
+
+@_spi(SendbirdInternal) public struct RequestParameter: Encodable {
+    let parameters: [String: Any]
+    private let encodeBlock: (Encoder) throws -> Void
+
+    @_spi(SendbirdInternal) public init() {
+        self.parameters = [:]
+        self.encodeBlock = { _ in }
+    }
+
+    @_spi(SendbirdInternal) public init<T: RequestCodingKey>(_ dict: [T: Encodable?]) {
+        let filteredDict = dict.compactMapValues { $0 }
+
+        self.parameters = filteredDict.mapKeysToString()
+        self.encodeBlock = { encoder in
+            var container = encoder.container(keyedBy: T.self)
+            for (key, value) in filteredDict {
+                try? container.encode(value, forKey: key)
+            }
+        }
+    }
+
+    @_spi(SendbirdInternal) public func encode(to encoder: Encoder) throws {
+        try encodeBlock(encoder)
+    }
+}
+
+extension RequestParameter {
+    /// To create RequestParameter with `CodeCodingKeys`, use this method.
+    static func param(_ dict: [CodeCodingKeys: Encodable?]) -> RequestParameter {
+        RequestParameter(dict)
+    }
+
+    /// Converts parameters to stringified dictionary for URL query encoding.
+    func stringify() -> [String: String] {
+        parameters.stringify()
+    }
+}
